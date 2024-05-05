057d52734bcf3b4908cefec4373a3015b3d821b4
Dutga-p <david.camilo.ordonez@correounivalle.edu.co>
2024-05-04 20:19:21 -0500

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login/Login';
+import { TodoWrapper } from './components/TodoWrapper';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/tabla" element={<TodoWrapper />} />
         </Routes>
       </Router>
   )
